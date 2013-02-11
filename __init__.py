66662b0df055d1aee61dc4931290e7374b0eefe8
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-02-11 14:57:35 +0100

    Release 1.2

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ def name():
 def description():
     return "Dockable dialog to find a feature by its ID in a layer."
 def version():
-    return "Version 1.1"
+    return "Version 1.2"
 def icon():
     return "icons/quickfinder.png"
 def qgisMinimumVersion():
