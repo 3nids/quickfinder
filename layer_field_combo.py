2813080b81e4012b1943030c4c9d10620af8edc0
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-02-27 15:29:02 +0100

    working layer field combo manager

--- layer_field_combo.py
+++ layer_field_combo.py
@@ -1,98 +1,110 @@
 """
-QGIS - Layer Field Combo class
+QGIS - Layer / Field Combos Manager
 
 Denis Rouzaud
 denis.rouzaud@gmail.com
 
-This class is useful if you want to simply manage a layer combo with one (or several) field combos.
-The field combos are filled with the column name of the currently selected layer in the layer combo.
+This class is useful if you want to simply manage a layer combo with one
+(or several) field combos. The field combos are filled with the names of
+columns of the currently selected layer in the layer combo.
 """
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from qgis.core import *
 
+########################################################################
 # creates a layer combo:
-# 	combo: the qcombobox widget
-#	geomType: restrain the possible selection of layers to a certain type of geometry [not working yet]
-#	initLayer: a lambda function returning the ID of the initially selected layer (it could look for a value in settings)
+#
+#    widget:      the qcombobox widget
+#
+#    initLayer:   the initally selected layer ID or a lambda function 
+#                 returning the ID (it could look for a value in settings)
+#
+#    hasGeometry: True/False, restrain the possible selection of layers
+#                 to layers having or not geometry 
+#
+#    geomType:    restrain the possible selection of layers to a certain
+#                 type of geometry geomType must be a GeometryType 
+#                 http://qgis.org/api/classQGis.html#a09947eb19394302eeeed44d3e81dd74b
+#
 class LayerCombo(QObject):
-	layerChanged = pyqtSignal()
-	
-	def __init__(self,iface,widget,geomType=None,initLayer=lambda:""):
+	def __init__(self, iface, widget, initLayer="", hasGeometry=None, geomType=None):
 		self.widget = widget
 		self.initLayer = initLayer
+		self.hasGeometry = hasGeometry
 		self.geomType = geomType
 		self.canvas = iface.mapCanvas()
 		QObject.__init__(self)
 		QObject.connect(self.canvas, SIGNAL("layersChanged ()") , self.canvasLayersChanged )
-		QObject.connect(widget, SIGNAL("currentIndexChanged(int)"), self.currentLayerChanged)
 		self.canvasLayersChanged()
-		
+        
 	def canvasLayersChanged(self):
 		self.widget.clear()
 		self.widget.addItem("")
+		if hasattr(self.initLayer,'__call__'):
+			initLayer = self.initLayer()
+		else:
+			initLayer = self.initLayer
 		for layer in self.canvas.layers():
-			if layer.type() != QgsMapLayer.VectorLayer:
-				continue
-			# TODO check geom  or layer.hasGeometryType() is False
+			# only select vector layer
+			if layer.type() != QgsMapLayer.VectorLayer:	continue
+			# if wanted, filter on hasGeometry
+			if self.hasGeometry is not None and layer.hasGeometry != self.hasGeometry: continue
+			# if wanted, filter on the geoetry type
+			if self.geomType is not None and layer.geometryType() != self.geomType:	continue
 			self.widget.addItem( layer.name() , layer.id() )
-			if layer.id() == self.initLayer(): self.combo.setCurrentIndex()
-			
+			if layer.id() == initLayer:
+				self.widget.setCurrentIndex( self.widget.count()-1 )
+            
 	def getLayer(self):
 		i = self.widget.currentIndex()
 		if i == 0: return None
 		layerId = self.widget.itemData( i ).toString()
 		return QgsMapLayerRegistry.instance().mapLayer( layerId )
-		
-	def currentLayerChanged(self,i):
-		self.layerChanged.emit()
-		print "layercombo: layer changed"
-		error_msg = ''
-		#if i > 0:
-		#	layer = self.layerList[i-1]
-		#	# TODO CHECK GEOMETRY
-		#	print "TODO CHECK GEOMETRY",layer.dataProvider().geometryType() , layer.geometryType()
-		#if error_msg != '':
-		#	self.dimensionLayerCombo.setCurrentIndex(0)
-		#	QMessageBox.warning( self , "Bad Layer", error_msg )
-		
 
 
 
+
+########################################################################
 # creates a field combo:
-# 	combo: the qcombobox widget
-#	fieldType: restrain the possible selection to a certain type of field
-#	initField: a lambda function returning the name of the initially selected field (it could look for a value in settings)
+#
+#    widget:    the qcombobox widget
+#
+#    layerCombo: the parent combobox defining the used layer
+#
+#    initField: the initially selected field name or a lambda function
+#               returning the name (it could look for a value in settings)
+#
+#    fieldType: restrain the possible selection to a certain type of field
+#
 class FieldCombo(QObject):
-	def __init__(self,widget,layerCombo,fieldType=None,initField=lambda:""):
+	def __init__(self, widget, layerCombo, initField="", fieldType=None):
 		self.widget = widget
 		self.layerCombo = layerCombo
 		self.initField = initField
 		self.fieldType = fieldType
 		QObject.__init__(self)
-		
 		QObject.connect(widget, SIGNAL("currentIndexChanged(int)"), self.fieldChanged)
-
-		QObject.connect(self.layerCombo.widget, SIGNAL("currentIndexChanged(int)"), self.layerChanged)
-		self.layerCombo.layerChanged.connect( self.layerChanged )
-		
+		QObject.connect(layerCombo.widget, SIGNAL("currentIndexChanged(int)"), self.layerChanged)
 		self.layerChanged()
 
 	def layerChanged(self):
-		print "fieldcombo: layer changed"
+		if hasattr(self.initField,'__call__'):
+			initField = self.initField()
+		else:
+			initField = self.initField
 		self.widget.clear()
 		self.widget.addItem("")
 		layer = self.layerCombo.getLayer()
 		if layer is None: return
 		for i,fieldItem in enumerate( layer.dataProvider().fieldNameMap() ):
 			self.widget.addItem( fieldItem )
-			if fieldItem == self.initField():
-				self.widget.setCurrentIndex(i+1)		
-				
+			if fieldItem == initField:
+				self.widget.setCurrentIndex(i+1)        
+                
 	def fieldChanged(self,i):
-		print "fieldcombo: field changed"
-		if i < 1 or checkType is None: return
+		if i < 1 or self.fieldType is None: return
 		layer = self.layerCombo.getLayer()
 		if layer is None: return
 		fieldName = self.widget.currentText()
@@ -100,6 +112,6 @@ class FieldCombo(QObject):
 		# http://qgis.org/api/classQgsField.html#a00409d57dc65d6155c6d08085ea6c324
 		# http://developer.qt.nokia.com/doc/qt-4.8/qmetatype.html#Type-enum
 		if self.getLayer().dataProvider().fields()[i].type() != self.fieldType:
-			QMessageBox.warning( self , "Bad field" ,  QApplication.translate("Layer Field Combo", "The field must be a %s" % self.fieldType, None, QApplication.UnicodeUTF8) )
+			QMessageBox.warning( self , "Field has an incorrect type" ,  QApplication.translate("Layer Field Combo", "The field must be a %s" % self.fieldType, None, QApplication.UnicodeUTF8) )
 			field.combo.setCurrentIndex(0)
-	
+
